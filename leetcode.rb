1032c1ac892a313574a5196734f48401f4305800
堤智春 <tsutsumitomoharu@tsutsumharusMBP.lan>
2023-05-07 11:03:12 -0700

    pivot_index

--- leetcode.rb
+++ leetcode.rb
@@ -7,3 +7,20 @@ def running_sum(nums)
   end
   new_arr
 end
+
+# https://leetcode.com/problems/find-pivot-index/?envType=study-plan&id=level-1
+
+def pivot_index(nums)
+  left = 0
+  right = 0
+  nums.each_with_index do |n, i|
+    left = i == 0 ? 0 : nums[0..(i-1)].sum
+    right = i == (nums.size-1) ? 0 : nums[i+1..nums.size-1].sum
+    if left == right
+      return i
+    end
+    if i == (nums.size - 1)
+      return -1
+    end
+  end
+end
